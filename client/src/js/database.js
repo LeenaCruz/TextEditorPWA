6f2451b59e2ecda3f9936f7fe6946aeb0910b9cc
Leena <leenacruz@gmail.com>
2024-09-09 13:51:32 -0700

    Changes to database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
   const jate = await openDB('jate', 1);
@@ -25,7 +25,7 @@ export const putDb = async (content) => {
 }
   // console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
+// Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all from database');
   const jateDb = await openDB('jate', 1);
@@ -36,6 +36,6 @@ const result = await request;
 console.log('result.value', result);
 return result;
 } 
-// console.error('getDb not implemented');
+
 
 initdb();
